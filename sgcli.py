b0f3319312842782273973dd6b2192ec468031b9
Michael Minichino <mminichino@mac.com>
2020-12-04 17:06:28 -0600

    Updates and Bug Fixes

--- sgcli.py
+++ sgcli.py
@@ -6,10 +6,16 @@ import getopt
 import getpass
 import sys
 import os
+import re
 import requests
 import urllib3
 import json
 import readline
+import datetime
+import signal
+import boto3
+import botocore
+from botocore.config import Config
 
 if sys.version_info < (3, 0):
     from urllib import urlencode
@@ -18,12 +24,17 @@ else:
 
 def usage():
     print("NetApp StorageGRID CLI")
-    print("Usage: " + sys.argv[0] + " [-l] [-v] [-o bucket/object] [-a admin_node_host]")
+    print("Usage: " + sys.argv[0] + " [-l] [-v] [-d] [-g] [-o bucket/object] [-a admin_node_host] [-e end_point] [-n node_search] [-p profile]")
     print("")
     print("-h  Print this message")
     print("-o  Lookup object bucket/object_name")
     print("-a  Hostname or IP address of the admin node")
     print("-l  Prompt for username and password, login and save auth token")
+    print("-e  Endpoint for bucket operations")
+    print("-p  AWS profile for authentication ($HOME/.aws/credentials")
+    print("-n  Limit results by node name")
+    print("-g  Display Grid Node metrics")
+    print("-d  Debug")
 
 def myinput(prompt, prefill):
 
@@ -62,6 +73,12 @@ def formatSize(bytes):
 
     return print_str
 
+def signal_handler(signal,frame):
+
+    print("")
+    print("Interrupt caught, exiting.")
+    sys.exit(0)
+
 class parse_args:
 
     def __init__(self):
@@ -69,13 +86,18 @@ class parse_args:
         self.arglist = []
         self.objectPath = None
         self.adminNode = None
+        self.awsProfile = None
+        self.searchNode = None
+        self.endPoint = None
         self.verboseFlag = False
         self.loginFlag = False
         self.formatFlag = False
+        self.debugFlag = False
+        self.gridMetrics = False
         self.argCount = 0
 
     def parse(self):
-        options, remainder = getopt.getopt(sys.argv[1:], 'fhvlo:a:', self.arglist)
+        options, remainder = getopt.getopt(sys.argv[1:], 'gdfhvlo:a:p:n:e:', self.arglist)
 
         self.argCount = len(options)
         for opt, arg in options:
@@ -83,12 +105,22 @@ class parse_args:
                 self.objectPath = arg
             elif opt in ('-a', '--adminnode'):
                 self.adminNode = arg
+            elif opt in ('-p', '--profile'):
+                self.awsProfile = arg
+            elif opt in ('-n', '--node'):
+                self.searchNode = arg
+            elif opt in ('-e', '--endpoint'):
+                self.endPoint = arg
             elif opt in ('-v', '--verbose'):
                 self.verboseFlag = True
             elif opt in ('-l', '--login'):
                 self.loginFlag = True
             elif opt in ('-f', '--format'):
                 self.formatFlag = True
+            elif opt in ('-d', '--debug'):
+                self.debugFlag = True
+            elif opt in ('-g', '--gridmetrics'):
+                self.gridMetrics = True
             elif opt in ('-h', '--help'):
                 usage()
                 sys.exit(0)
@@ -103,11 +135,24 @@ class auth_token:
         self.argset = argclass
         self.adminNode = self.argset.adminNode
         self.verboseFlag = self.argset.verboseFlag
+        self.debugFlag = self.argset.debugFlag
         self.formatFlag = self.argset.formatFlag
+        self.gridMetrics = self.argset.gridMetrics
+        self.searchNode = self.argset.searchNode
         self.authToken = None
         self.homeDir = os.environ.get('HOME')
         self.authPath = self.homeDir + '/.storagegrid'
         self.authFile = self.authPath + '/auth'
+        self.awsProfile = self.argset.awsProfile
+        self.endPoint = self.argset.endPoint
+        try:
+            if self.awsProfile:
+                self.s3session = boto3.Session(profile_name=self.awsProfile,)
+            else:
+                self.s3session = boto3.Session(profile_name="default",)
+        except botocore.exceptions.ProfileNotFound as e:
+            print("Error: %s" % str(e))
+            sys.exit(1)
 
     def genToken(self):
 
@@ -173,7 +218,19 @@ class storagegrid:
         self.authToken = self.token.authToken
         self.adminName = self.token.adminNode
         self.verboseFlag = self.token.verboseFlag
+        self.debugFlag = self.token.debugFlag
+        self.gridMetrics = self.token.gridMetrics
         self.formatFlag = self.token.formatFlag
+        self.searchNode = self.token.searchNode
+        self.endPoint = self.token.endPoint
+        self.bucketObjects = []
+        config = Config(
+            retries={
+                'max_attempts': 10,
+                'mode': 'standard'
+            }
+        )
+        self.s3 = self.token.s3session.client('s3', endpoint_url=self.endPoint, verify=False, config=config)
 
     def objectLookup(self, lookup):
 
@@ -206,6 +263,9 @@ class storagegrid:
         response = requests.get(url, headers=headers, verify=False)
         self.grid_data = json.loads(response.text)
 
+        if self.debugFlag:
+            print(json.dumps(self.grid_data, indent=2))
+
         node_list = {}
         site_name = ''
         grid_name = ''
@@ -235,7 +295,7 @@ class storagegrid:
         self.grid_node_list = node_list
         self.grid_name = grid_name
 
-    def objectList(self, lookup):
+    def objectList(self, lookup, node=None):
 
         self.objectLookup(lookup)
         self.getGridTopology()
@@ -246,47 +306,118 @@ class storagegrid:
         objsize = ''
         objrep = ''
         objbucket = ''
+        objmodtime = ''
 
         if self.verboseFlag:
             print(json.dumps(self.object_data, indent=2))
         else:
-            for key in self.object_data:
-                if key == "data":
-                    for subkey in self.object_data[key]:
-                        if subkey == "name":
-                            objname = self.object_data[key][subkey]
-                        if subkey == "objectSizeBytes":
-                            objsize = formatSize(self.object_data[key][subkey])
-                        if subkey == "container":
-                            objbucket = self.object_data[key][subkey]
-                        if subkey == "locations":
-                            for x in range(len(self.object_data[key][subkey])):
-                                if self.object_data[key][subkey][x]['type'] == "erasureCoded":
-                                    objrep = "EC"
-                                    for y in range(len(self.object_data[key][subkey][x]['fragments'])):
-                                        objlocation.append(self.grid_node_list[self.object_data[key][subkey][x]['fragments'][y]['nodeId']]['name'])
-                                        objsegtype.append(self.object_data[key][subkey][x]['fragments'][y]['type'])
-                                if self.object_data[key][subkey][x]['type'] == "replicated":
-                                    objrep = "Copy"
-                                    objlocation.append(self.grid_node_list[self.object_data[key][subkey][x]['nodeId']]['name'])
-                                    objsegtype.append("copy")
+            if 'data' in self.object_data:
+                objname = self.object_data['data']['name']
+                objsize = formatSize(self.object_data['data']['objectSizeBytes'])
+                objbucket = self.object_data['data']['container']
+                objmodtime = self.object_data['data']['modifiedTime']
+                date_time = datetime.datetime.strptime(objmodtime, '%Y-%m-%dT%H:%M:%S.%fZ')
+                objmodtime = date_time.strftime("%m/%d/%y %I:%M%p")
+                for x in range(len(self.object_data['data']['locations'])):
+                    if self.object_data['data']['locations'][x]['type'] == "erasureCoded":
+                        objrep = "EC"
+                        for y in range(len(self.object_data['data']['locations'][x]['fragments'])):
+                            objlocation.append(self.grid_node_list[self.object_data['data']['locations'][x]['fragments'][y]['nodeId']]['name'])
+                            objsegtype.append(self.object_data['data']['locations'][x]['fragments'][y]['type'])
+                    if self.object_data['data']['locations'][x]['type'] == "replicated":
+                        objrep = "Replicated"
+                        objlocation.append(self.grid_node_list[self.object_data['data']['locations'][x]['nodeId']]['name'])
+                        objsegtype.append("copy")
+
+            if self.searchNode:
+                if self.searchNode not in objlocation:
+                    return
 
             if self.formatFlag:
-                print("%s,%s,%s,%s," % (objbucket, objname, objsize, objrep), end='')
+                print("%s,%s,%s,%s,%s" % (objbucket, objname, objsize, objmodtime, objrep), end='')
                 for x in range(len(objlocation)):
                     if x == len(objlocation) - 1:
                         print("%s,%s" % (objsegtype[x], objlocation[x]))
                     else:
                         print("%s,%s," % (objsegtype[x], objlocation[x]), end='')
             else:
-                print("[%s]/%s %s %s " % (objbucket, objname, objsize, objrep), end='')
+                print("[%s]/%s %s %s %s " % (objbucket, objname, objsize, objmodtime, objrep), end='')
                 for x in range(len(objlocation)):
                     print("%s => %s " % (objsegtype[x], objlocation[x]), end='')
                 print("")
 
+    def listBucket(self, lookup):
+
+        bucket_name, obj_pattern = lookup.split('/')
+        obj_pattern = re.sub('\*', '.+', obj_pattern)
+        obj_pattern = '^' + obj_pattern + '$'
+
+        try:
+            bucket_region = self.s3.get_bucket_location(Bucket=bucket_name)
+            self.bucketRegion = bucket_region
+        except RecursionError as e:
+            print("Error: connection to %s failed." % self.endPoint)
+            sys.exit(1)
+
+        try:
+            kwargs = {'Bucket': bucket_name}
+            while True:
+                block = self.s3.list_objects_v2(**kwargs)
+                for obj_entry in block['Contents']:
+                    if re.search(obj_pattern, obj_entry['Key']):
+                        self.objectList(bucket_name + '/' + obj_entry['Key'])
+                if block['IsTruncated']:
+                    kwargs['ContinuationToken'] = block['NextContinuationToken']
+                else:
+                    break
+        except (botocore.exceptions.ClientError, botocore.exceptions.EndpointConnectionError) as e:
+            print("Error: can not connect to bucket %s: %s" % (bucket_name,str(e)))
+            sys.exit(1)
+
+    def gridStatus(self):
+
+        headers = {}
+
+        headers.update({ 'accept' : 'application/json' })
+        headers.update({ 'Authorization' : 'Bearer ' + self.authToken })
+
+        url = 'https://' + self.adminName + '/api/v3/grid/metric-query?query=storagegrid_storage_utilization_total_space_bytes&timeout=120s'
+
+        response = requests.get(url, headers=headers, verify=False)
+        self.grid_total_space = json.loads(response.text)
+
+        url = 'https://' + self.adminName + '/api/v3/grid/metric-query?query=storagegrid_storage_utilization_usable_space_bytes&timeout=120s'
+
+        response = requests.get(url, headers=headers, verify=False)
+        self.grid_usable_space = json.loads(response.text)
+
+        if self.debugFlag:
+            print(json.dumps(self.grid_total_space, indent=2))
+            print(json.dumps(self.grid_usable_space, indent=2))
+
+        self.getGridTopology()
+
+        print("%s %s %s %s" % (str("Node Name").ljust(16),
+                                 str("Total Space").ljust(14),
+                                 str("Free Space").ljust(14),
+                                 str("Used Percent")))
+
+        for x in range(len(self.grid_total_space['data']['result'])):
+            node_name = self.grid_node_list[self.grid_total_space['data']['result'][x]['metric']['node_id']]['name']
+            node_total = self.grid_total_space['data']['result'][x]['value'][1]
+            node_usable = self.grid_usable_space['data']['result'][x]['value'][1]
+            node_pct_full = int(node_usable) / int(node_total)
+            node_pct_full = round(100 - (node_pct_full * 100))
+
+            print("%s %s %s %d%%" % (str(node_name).ljust(16),
+                                     str(formatSize(int(node_total))).ljust(14),
+                                     str(formatSize(int(node_usable))).ljust(14),
+                                     node_pct_full))
+
 def main():
 
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+    signal.signal(signal.SIGINT, signal_handler)
 
     runargs = parse_args()
     runargs.parse()
@@ -300,7 +431,13 @@ def main():
 
     myGrid = storagegrid(myToken)
 
-    if runargs.objectPath:
+    if runargs.gridMetrics:
+        myGrid.gridStatus()
+        sys.exit(0)
+
+    if runargs.endPoint and runargs.objectPath:
+        myGrid.listBucket(runargs.objectPath)
+    elif runargs.objectPath:
         myGrid.objectList(runargs.objectPath)
 
 if __name__ == '__main__':
